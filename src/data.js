9f2d9771bc3de518b604da33525e1d70d1635acc
susanita12352 <scahuantico@gmail.com>
2019-03-25 16:20:50 -0500

    actualizando el main

--- src/data.js
+++ src/data.js
@@ -1,35 +1,15 @@
-/* const poki = POKEMON.pokemon;
-for (i=0; i< poki.length ; i++){
-console.log(poki[i].name)
-} 
-*/
-const root = document.getElementById('root');
+const dataPokemon = () => {
+  return POKEMON.pokemon;
+};
 
-const filter = () => {
-  for (let i=0; i<filter.length; i++);
-  }
+const filterAz = () => {
+  const orderAZ = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I','J', 'K', 'L','M', 'N', 'O', 'P','Q', 'R', 'S', 'T','U','V','W','X','Y', 'Z'];
+  for (let i=0; i<POKEMON.pokemon.length; i++){
 
-// filter(POKEMON.pokemon)
-
-
-const poki = (pokedex) => {
-  for (let i = 0; i < POKEMON.pokemon.length; i++) {
-    root.innerHTML += `
-      <div>
-        <p>Number: ${pokedex[i].id}</p>
-        <p>Name: ${pokedex[i].name}</p>
-        <p>Type: ${pokedex[i].type}</p>
-        <p>height: ${pokedex[i].height}</p>
-        <p>weight: ${pokedex[i].weight}</p>
-        <p>candy: ${pokedex[i].candy}</p>
-        <p>candy_count: ${pokedex[i].candy_count}</p>
-        <p>egg: ${pokedex[i].egg}</p>
-        <figure>
-          <img src="${pokedex[i].img}" alt="perfil" class="img-profile">
-       </figure>
-      </div> 
-    `;
+    console.log(POKEMON.pokemon[i].name)
   }
 }
 
-poki(POKEMON.pokemon);
+filterAz();
+
+window.dataPokemon = dataPokemon;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
 /* Manejo del DOM */
+const root = document.getElementById('root');
+const poki = (pokedex) => {
+  for (let i = 0; i < pokedex.length; i++) {
+    root.innerHTML += `
+      <div>
+        <p>Number: ${pokedex[i].id}</p>
+        <p>Name: ${pokedex[i].name}</p>
+        <p>Type: ${pokedex[i].type}</p>
+        <p>height: ${pokedex[i].height}</p>
+        <p>weight: ${pokedex[i].weight}</p>
+        <p>candy: ${pokedex[i].candy}</p>
+        <p>candy_count: ${pokedex[i].candy_count}</p>
+        <p>egg: ${pokedex[i].egg}</p>
+        <figure>
+          <img src="${pokedex[i].img}" alt="perfil" class="img-profile">
+       </figure>
+      </div> 
+    `;
+  }
+}
 
+poki(window.dataPokemon())
 
